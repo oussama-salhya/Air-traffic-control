5279fae103c8a8a4a04cd3a38cf87113720ffbee
driss <jabri.dri.fst@uhp.ac.ma>
2023-12-02 15:51:14 +0100

    first commit

---
+++ src/main/java/com/example/controle_aerien/ControleAerienApplication.java
@@ -0,0 +1,13 @@
+package com.example.controle_aerien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAerienApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleAerienApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/controle_aerien/ControleAerienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controle_aerien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAerienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
