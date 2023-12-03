3c4acc44bc27faa02e474584797448f8dee33d43
oussama-salhya <you@example.com>
2023-12-03 12:55:05 +0100

    adding repo

---
+++ src/main/java/com/example/controle_aerien/dao/DistanceAeroportRepository.java
@@ -0,0 +1,9 @@
+package com.example.controle_aerien.dao;
+
+import com.example.controle_aerien.entities.DistanceAeroport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DistanceAeroportRepository extends JpaRepository<DistanceAeroport, Long> {
+
+}
+
